70cd09971a8eeb946ae54f1f74d748b4ea41c5ba
xiuye <xiuye_engineer@outlook.com>
2016-11-13 10:59:52 +0800

    spring mvc training

---
+++ src/com/xiuye/web/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.xiuye.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan(basePackages = { "com.xiuye.web" }, excludeFilters = {
+//		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+
+}
---
+++ src/com/xiuye/web/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.xiuye.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.xiuye.web.controller") // 必须在此种类中开启包扫描,否则无效
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+
+		// 开启静态资源的请求转发到默认servlet上,不配置页面报错404,(默认servlet不是DispatcherServlet!理解的)
+		configurer.enable();
+
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/views/");
+		resolver.setSuffix(".html");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+
+	}
+
+}
