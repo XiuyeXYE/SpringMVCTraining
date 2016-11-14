ee22376cdf3562f2edfa3828a44a6b9f29fb09c2
xiuye <xiuye_engineer@outlook.com>
2016-11-14 22:23:21 +0800

    new

--- src/com/xiuye/web/servlet/FirstServlet.java
+++ src/com/xiuye/web/servlet/FirstServlet.java
@@ -13,20 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(description = "first servlet demo", urlPatterns = { "/FirstServlet" })
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public FirstServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -34,7 +32,6 @@ public class FirstServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
